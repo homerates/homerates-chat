be33cc9588b60aebc5c50d1e5e8c2a771de42995
HomeRates Automator <dev@homerates.ai>
2025-09-30 22:00:41 -0700

    Diag: replace index.html with inline version check 20250930-220040

--- api/version.js
+++ api/version.js
@@ -1,3 +1,3 @@
 ﻿module.exports = async function (req, res) {
-  res.status(200).json({ ok:true, project:'homerates-chat', version:'20250930-215057', host: req.headers.host || '' });
+  res.status(200).json({ ok:true, project:'homerates-chat', version:'20250930-220040', host: req.headers.host || '' });
 };
